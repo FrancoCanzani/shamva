46153ace6d70dce0b186854bb5c0b4dc538e0197
Franco Canzani <francocanzani@gmail.com>
2025-07-30 23:15:21 +0200

    feat(web): Add animation duration to loading spinner

--- apps/web/src/frontend/components/loading.tsx
+++ apps/web/src/frontend/components/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <Loader className="animate-spin" />
+      <Loader className="animate-spin duration-75" />
     </div>
   );
 }
